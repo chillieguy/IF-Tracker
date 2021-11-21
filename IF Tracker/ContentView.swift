01b182161f0ab177b52d79f1a42881b98f45ed14
Chuck Underwood <chillieguy@live.com>
2021-11-21 08:49:47 -0800

    Initial Commit

---
+++ IF Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IF Tracker
+//
+//  Created by Chuck Underwood on 11/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IF Tracker/IF_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IF_TrackerApp.swift
+//  IF Tracker
+//
+//  Created by Chuck Underwood on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct IF_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
